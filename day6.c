5958bbe3905e999e7557d19a6a805ea24080cc1d
Siddeshwar <siddeshwar.work@gmail.com>
2024-07-18 19:38:16 +0530

    completed the calculation

--- day6.c
+++ day6.c
@@ -27,29 +27,37 @@ char *trimStr(char *s) {
   return o;
 }
 
-void extractData(char *line) {
+int *extractData(char *line) {
   line = strstr(line, ":");
   line = strstr(line, " ");
   line = trimStr(line);
 
   char *token;
   int i = 0;
+  int *o = (int *)malloc(1 * sizeof(int));
 
   const char c[] = " ";
   token = strtok(line, c);
 
   while (token != NULL) {
     int val = atoi(token);
-    printf("%d\n", val);
+
+    o = (int *)realloc(o, (i + 1) * sizeof(int));
+    o[i] = val;
 
     token = strtok(NULL, c);
     i++;
   }
+
+  return o;
 }
 
 int main() {
   FILE *filePtr = fopen("day6.txt", "r");
   char *line;
+  int *timeData = NULL, *distanceData = NULL;
+  int result = 1;
+  const int size = 3; // TODO: get this dynamic
 
   if (filePtr == NULL) {
     printf("Failed to read the file");
@@ -57,9 +65,32 @@ int main() {
   }
 
   fgets(line, 50, filePtr);
-  extractData(line);
+  timeData = extractData(line);
+
+  fgets(line, 50, filePtr);
+  distanceData = extractData(line);
+
+  for (int i = 0; i < size; i++) {
+    int count = 0;
+    int holdTime = 1;
+
+    while (holdTime < timeData[i]) {
+      int distance = holdTime * (timeData[i] - holdTime);
+
+      if (distance > distanceData[i]) {
+        count++;
+      }
+      holdTime++;
+    }
+
+    result *= count;
+  }
+
+  printf("%d", result);
 
   fclose(filePtr);
+  free(timeData);
+  free(distanceData);
 
   return 0;
 }
